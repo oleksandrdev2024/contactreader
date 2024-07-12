7e8a55fa755f96b5c86f2b4622a48b587241acf5
smartywebdev0119 <smartwebdev0119@gmail.com>
2024-07-12 14:59:03 -0400

    update ui

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -158,8 +158,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-900">
-      <div className="w-[200px] flex justify-center gap-2 text-white">
-        <p>Loading Emails... {readEmailCount}</p>
+      <div className="w-[300px] flex justify-center gap-2 text-white">
+        <p> {`Loading Emails... ${readEmailCount}`}</p>
       </div>
     </main>
   );
