98bd65095606805b9639737305dd6d05bb93d17c
smartywebdev0119 <smartwebdev0119@gmail.com>
2024-07-11 22:08:04 -0400

    getting emails to frontend

--- app/auth/outlook/callback/page.tsx
+++ app/auth/outlook/callback/page.tsx
@@ -1,51 +1,86 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  const [accessToken, setAccessToken] = useState<string>("");
-
-  const fetchEmails = async (token: string) => {
-    const res = await fetch("/api/download-csv/outlook", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ accessToken: token }),
-    });
-
-    if (res.status === 200) {
-      const blob = await res.blob();
-
-      const link = document.createElement("a");
-      link.href = window.URL.createObjectURL(blob);
-      link.download = "data.csv";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      window.location.href =
-        process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
-    }
-  };
+  const [accessToken, setAccessToken] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("accessToken");
-    if (token && accessToken === "") {
+    if (token) {
       setAccessToken(token);
+      if (accessToken === "") {
+        fetchEmails(token);
+      }
     }
   }, []);
 
-  useEffect(() => {
-    if (accessToken !== "") {
-      fetchEmails(accessToken);
+  const fetchEmails = async (token: string) => {
+    try {
+      const userDetails = await fetch("https://graph.microsoft.com/v1.0/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res) => res.json());
+
+      let path =
+        "https://graph.microsoft.com/v1.0/me/messages?$top=500&$select=from,toRecipients";
+      const count: any = {};
+
+      while (path) {
+        const graphResponse = await fetch(path, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((res) => res.json());
+
+        graphResponse.value.forEach((d: any) => {
+          let email = "";
+          if (d.from.emailAddress.address === userDetails.mail) {
+            email = d.toRecipients[0].emailAddress.address;
+          } else if (
+            d.toRecipients[0].emailAddress.address === userDetails.mail
+          ) {
+            email = d.from.emailAddress.address;
+          }
+          if (email) {
+            count[email] = (count[email] ?? 0) + 1;
+          }
+        });
+
+        path = graphResponse["@odata.nextLink"];
+      }
+
+      generateCSV(count, userDetails.mail);
+    } catch (err) {
+      console.error("Error fetching emails:", err);
+      setLoading(false);
     }
-  }, [accessToken]);
+  };
+
+  const generateCSV = (count: any, userEmail: any) => {
+    const csvRows = ["Email, Count"];
+    Object.entries(count).forEach(([email, count]) => {
+      csvRows.push(`${email},${count}`);
+    });
+
+    const blob = new Blob([csvRows.join("\n")], { type: "text/csv" });
+
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = `${userEmail}.csv`;
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    window.location.href =
+      process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-900">
       <div className="w-[200px] flex justify-center gap-2">
-        <p>Loading Emails</p>
+        {loading ? <p>Loading Emails...</p> : <p>Emails downloaded.</p>}
       </div>
     </main>
   );
